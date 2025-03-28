41de24fdc1f01027a6b1ed12035dbb10188a2ef3
Prathamesh-Patkal <161063093+Prathamesh-Patkal@users.noreply.github.com>
2025-03-28 15:27:17 +0530

    Add files via upload

---
+++ Assignment-4/src/app/app.routes.ts
@@ -0,0 +1,5 @@
+import { Routes } from '@angular/router';
+import {LoginComponent} from './login/login.component'
+import {RegistrationComponent} from './registration/registration.component'
+
+export const routes: Routes = [{path : "registration" , component:RegistrationComponent},{path:"login",component:LoginComponent},{path:"",redirectTo:"login",pathMatch:"full"}];
